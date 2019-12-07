4a76c2a5428ebaf050746f193f7ed66c026623f6
Yair Fernando Facio Fraga <yair.facio11@gmail.com>
2019-12-07 16:55:50 -0600

    Adding some styles to the VideoItem component

---
+++ src/components/VideoList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import VideoItem from '../components/VideoItem';
+
+const VideoList = ({ videos }) => {
+  const renderList = videos.map((video) => {
+    return <VideoItem video={video} />
+  })
+
+  return(
+    <div className="ui relaxed divided list">{renderList}</div>
+  );
+};
+
+export default VideoList;
